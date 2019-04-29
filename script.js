73770dfd4ee81da1fafead00d1515317efcdaf79
Laura Macharia <lauramacharia117@gmail.com>
2019-04-29 09:55:16 +0300

    add changes to script.js

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-function triangle(){
-    var base= parseInt(getElementById("base").valueOf);
-    var hypotenuse = parseInt(getElementById("hypotenuse").valueOf);
-    var height = parseInt(getElementById("height").valueOf);
-    var answer = ("answer");
-    if(hypotenuse === height && hypotenuse === base && base === height){
-    /*alert("Equilateral");*/
+function triangle() {
+    var base= parseInt(getElementById("base").value);
+    var hypotenuse = parseInt(getElementById("hypotenuse").value);
+    var height = parseInt(getElementById("height").value);
+    var text;
+    var sides = [base ,hypotenuse,height];
+    if (base <= 0 || hypotenuse <=0 || height <= 0){
+    text = "positive intergers";
+    }
+   else if(hypotenuse === height && hypotenuse === base && base === height){
+    text = "Equilateral";
     }
     else if(base === hypotenuse || hypotenuse === height || base === height){
-    /*alert("Isosceles");*/
+    text = "Isosceles";
     }
     else if((height + hypotenuse) <= base || (hypotenuse + base) <= height || (base + height) <= hypotenuse) {
-    /*alert("Not a definite triangle");*/
+    text = "Not a definite triangle";
+    }
+    else if(base !== hypotenuse || hypotenuse !== height || height !== base){
+    text ="scalene";
     }
     else{
-    alert("Scalene");
+        text = "error"
     }
+    document.getElementById("triangles").innerHTML = text
 }
     
